0bceedf64b88b06ca70e96d1a5e40bbd7b0437e9
mayurnilwant <mayurnilwant@gmail.com>
2022-09-27 17:43:28 +0100

    Initial Commit

---
+++ HttpClientApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HttpClientApp
+//
+//  Created by Mayur Nilwant on 27/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HttpClientApp/HttpClientAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HttpClientAppApp.swift
+//  HttpClientApp
+//
+//  Created by Mayur Nilwant on 27/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct HttpClientAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
