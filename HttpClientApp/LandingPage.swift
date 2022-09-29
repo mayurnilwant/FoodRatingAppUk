f391df0afc3ba32e1efecbbd0ffd93dec546b3e7
mayurnilwant <mayurnilwant@gmail.com>
2022-09-29 11:47:00 +0100

    Changed name of file.

--- HttpClientApp/LandingPage.swift
+++ HttpClientApp/LandingPage.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LandingPageView: View {
     
     init() {
         
@@ -29,6 +29,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LandingPageView()
     }
 }
